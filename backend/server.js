ebf33dcafd397e9faa831d8c62cc6ba2b5f2b03e
Dhruvil <dhruvilpatel.m@gmail.com>
2025-12-11 01:38:20 +0530

    Refactor server setup to include CORS and improved request handling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,21 @@
-import express from "express" ;
+import express, { Router } from "express" ;
+import cors from "cors" ;
+import 'dotenv/config'
+import { connectDB } from "./config/db.js";
 
 const app = express() ;
 const port = process.env.PORT || 4000 ;
 
+app.use(cors()) ;
 app.use(express.json()) ;
+app.use(express.urlencoded({extended: true})) ;
+
+connectDB() ;
+
+app.get('/' , (req , res)=>{
+    res.send("API working") ;
+})
+
+app.listen(port , ()=> {
+    console.log(`Server started on http://localhost:${port}`)
+})
