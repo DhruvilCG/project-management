9c11e0215795421df406a9422f75cfb5c4feb58c
Dhruvil <dhruvilpatel.m@gmail.com>
2025-12-12 12:37:46 +0530

    Fix import path for User model in userController and auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-import User from "../models/usermodel.js";
+import User from "../models/userModel.js";
 import validator from "validator" ;
 import bcrypt from "bcryptjs" ;
 import jwt from 'jsonwebtoken' ;
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken' ;
-import User from "../models/usermodel.js";
+import User from "../models/userModel.js";
 
 const JWT_SECRET = process.env.JWT_SECRET ;
 
